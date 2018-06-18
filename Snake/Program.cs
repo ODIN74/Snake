cb5e2b4dc086cc14252676ac251343e1c4f277cd
ODIN74 <fantompus@gmail.com>
2018-06-18 13:04:57 +0500

    Исправления в оформлении кода

--- Snake/Program.cs
+++ Snake/Program.cs
@@ -11,19 +11,20 @@ namespace Snake
         static void Main(string[] args)
         {
             Point p1 = new Point(1,3,'*');
-
-
-
+            p1.Draw();
+            
             Point p2 = new Point(4, 5, '#');
-
-
-
+            p2.Draw();
+            
             HorizontalLine hLineTop = new HorizontalLine(1, Console.WindowWidth - 2, 0, '-');
             hLineTop.Draw();
+
             VerticalLine vLineLeft = new VerticalLine(0, 1, Console.WindowHeight - 2, '|');
             vLineLeft.Draw();
+
             HorizontalLine hLineBottom = new HorizontalLine(1, Console.WindowWidth - 2, Console.WindowHeight - 1, '-');
             hLineBottom.Draw();
+
             VerticalLine vLineRight = new VerticalLine(Console.WindowWidth - 1, 1, Console.WindowHeight - 2, '|');
             vLineRight.Draw();
 
