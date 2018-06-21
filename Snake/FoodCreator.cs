032024cb1364ab09ebe206821dc710adc5d208c6
ODIN74 <fantompus@gmail.com>
2018-06-21 16:53:06 +0500

    Добавление класса стен.

---
+++ Snake/FoodCreator.cs
@@ -0,0 +1,70 @@
+﻿//-----------------------------------------------------------------------
+// <copyright file="FoodCreator.cs" company="Anton Klyushin">
+// Copyright (c) Anton Klyushin. All rights reserved.
+// </copyright>
+//-----------------------------------------------------------------------
+namespace Snake
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+    using System.Threading.Tasks;
+
+    /// <summary>
+    /// Create food by snake
+    /// </summary>
+    internal class FoodCreator
+    {
+        /// <summary>
+        /// Width of the map
+        /// </summary>
+        private readonly int mapWidth;
+
+        /// <summary>
+        /// Height of the map
+        /// </summary>
+        private readonly int mapHeight;
+
+        /// <summary>
+        /// Symbol of food
+        /// </summary>
+        private readonly char symbol;
+
+        /// <summary>
+        /// Randomizer of food coordinates
+        /// </summary>
+        private Random rnd = new Random();
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="FoodCreator"/> class.
+        /// </summary>
+        /// <param name="mapWidth">Width of the map</param>
+        /// <param name="mapHeight">Height of the map</param>
+        /// <param name="symbol">Symbol of food</param>
+        public FoodCreator(int mapWidth, int mapHeight, char symbol)
+        {
+            this.mapWidth = mapWidth;
+            this.mapHeight = mapHeight;
+            this.symbol = symbol;
+        }
+
+        /// <summary>
+        /// Create food point
+        /// </summary>
+        /// <param name="snake">Snake instance</param>
+        /// <returns>Food Point</returns>
+        internal Point CreateFood(Snake snake)
+        {
+            int x;
+            int y;
+            do
+            {
+                x = this.rnd.Next(2, this.mapWidth - 3);
+                y = this.rnd.Next(2, this.mapHeight - 2);
+            }
+            while (snake.FoodOnSnake(x, y));
+            return new Point(x, y, this.symbol);
+        }
+    }
+}
---
+++ Snake/Walls.cs
@@ -0,0 +1,73 @@
+﻿//-----------------------------------------------------------------------
+// <copyright file="Walls.cs" company="Anton Klyushin">
+// Copyright (c) Anton Klyushin. All rights reserved.
+// </copyright>
+//-----------------------------------------------------------------------
+namespace Snake
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+    using System.Threading.Tasks;
+
+    /// <summary>
+    /// Walls class
+    /// </summary>
+    internal class Walls
+    {
+        /// <summary>
+        /// List of walls
+        /// </summary>
+        private readonly List<Figure> wallList;
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="Walls"/> class.
+        /// </summary>
+        /// <param name="mapWidth">Width of map</param>
+        /// <param name="mapHeight">Height of map</param>
+        internal Walls(int mapWidth, int mapHeight)
+        {
+            this.wallList = new List<Figure>();
+
+            var horizontalLineTop = new HorizontalLine(0, Console.WindowWidth - 2, 0, '*');
+            var verticalLineLeft = new VerticalLine(0, 1, Console.WindowHeight - 2, '*');
+            var horizontalLineBottom = new HorizontalLine(0, Console.WindowWidth - 2, Console.WindowHeight - 1, '*');
+            var verticalLineRight = new VerticalLine(Console.WindowWidth - 2, 1, Console.WindowHeight - 2, '*');
+
+            this.wallList.Add(horizontalLineTop);
+            this.wallList.Add(horizontalLineBottom);
+            this.wallList.Add(verticalLineLeft);
+            this.wallList.Add(verticalLineRight);
+        }
+
+        /// <summary>
+        /// Hit with figure
+        /// </summary>
+        /// <param name="fig">Figure instance</param>
+        /// <returns>True or False</returns>
+        internal bool IsHit(Figure fig)
+        {
+            foreach (var wall in this.wallList)
+            {
+                if (wall.IsHit(fig))
+                {
+                    return true;
+                }
+            }
+
+            return false;
+        }
+
+        /// <summary>
+        /// Draw walls method
+        /// </summary>
+        internal void Draw()
+        {
+            foreach (var wall in this.wallList)
+            {
+                wall.Draw();
+            }
+        }
+    }
+}
